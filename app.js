733ae505558bbb222b65ba16b667a69852925b9f
Swati <93772681+StarGrayy@users.noreply.github.com>
2024-06-12 09:04:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function calculateBill(){
-
     const bill = parseFloat(document.getElementById('bill').value);
     const tip = parseFloat(document.getElementById('tip').value);
+    
+    if(!isNaN(bill) && !isNaN(tip)){
     const ans=bill+(bill*tip)/100;
-
     document.getElementById('result').innerText=`Your Bill is ${ans}`;
+    }else{
+    document.getElementById('result').innerText="Please Enter Valid Amount";  
+    }
 } 
